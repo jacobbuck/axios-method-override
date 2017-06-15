eed0bec7b5ec88f91fc1a4625e31c5369aaf8bd9
Jacob Buck <buck.jacob@gmail.com>
2017-06-15 13:17:11 +1200

    Prettier

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-'use strict';
+"use strict";
 
-var assign = require('object-assign');
+var assign = require("object-assign");
 
 module.exports = function(axios) {
   axios.interceptors.request.use(function(config) {
     var method = config.method.toUpperCase();
-    if (method === 'PUT' || method === 'DELETE' || method === 'PATCH') {
-      config.headers['X-HTTP-Method-Override'] = method;
-      config.method = 'post';
+    if (method === "PUT" || method === "DELETE" || method === "PATCH") {
+      config.headers["X-HTTP-Method-Override"] = method;
+      config.method = "post";
       config.params = assign({}, config.params, { _method: method });
     }
     return config;
