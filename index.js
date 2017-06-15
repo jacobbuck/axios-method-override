fc32c4fe7b83f693f95382a3ed94379979776ff6
Jacob Buck <buck.jacob@gmail.com>
2017-06-15 13:16:10 +1200

    Add object-assign. Use object-assign for updating config.params

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 'use strict';
 
+var assign = require('object-assign');
+
 module.exports = function(axios) {
   axios.interceptors.request.use(function(config) {
     var method = config.method.toUpperCase();
     if (method === 'PUT' || method === 'DELETE' || method === 'PATCH') {
       config.headers['X-HTTP-Method-Override'] = method;
       config.method = 'post';
-      config.params._method = method;
+      config.params = assign({}, config.params, { _method: method });
     }
     return config;
   });
