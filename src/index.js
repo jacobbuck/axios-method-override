51b4056a85decaeb5b906eb9e5a9eb703b681a69
Jacob Buck <buck.jacob@gmail.com>
2017-07-29 12:12:47 +1200

    ES6ify. Add babel devDeps and build script

---
+++ src/index.js
@@ -0,0 +1,18 @@
+const axiosMethodOverride = axios => {
+  axios.interceptors.request.use(config => {
+    const method = config.method.toUpperCase();
+    if (method === "PUT" || method === "DELETE" || method === "PATCH") {
+      config.headers = {
+        ...config.headers,
+        ["X-HTTP-Method-Override"]: method,
+      };
+      config.method = "post";
+      config.params = {
+        ...config.params,
+        _method: method,
+      };
+    }
+    return config;
+  });
+  return axios;
+};
